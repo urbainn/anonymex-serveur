9d2e9906e670f8b7a7b6e827018569eef5fb28ad
urbainn <ubrainfr@gmail.com>
2025-10-23 16:29:20 +0200

    lecture bordereau WIP

---
+++ src/lecture/preparation/genererCanvasLecture.ts
@@ -0,0 +1,9 @@
+/**
+ * Créé un canvas contenant le scan d'un scan sous forme PNG ou PDF.
+ */
+
+import { Canvas } from "canvas";
+
+export default function genererCanvasLecture(/* à déterminer: type d'entrée. */): Canvas {
+
+}

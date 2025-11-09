0b2e7d2c63f88264a37e4fb8c9b478d8b39ae640
antoinedmns <antoine.damiens1@orange.fr>
2025-11-09 21:36:02 +0100

    correction index utilisateurs

--- src/routes/utilisateurs/index.ts
+++ src/routes/utilisateurs/index.ts
@@ -10,10 +10,10 @@ const utilisateursRouter = Router();
 // GET /utilisateurs/
 utilisateursRouter.get("/", (req, res) => useRest(getUtilisateurs, req, res));
 // POST /utilisateurs/login
-utilisateursRouter.get("/login", (req, res) => useRest(postUtilisateur, req, res));
+utilisateursRouter.post("/login", (req, res) => useRest(postUtilisateur, req, res));
 // PATCH /utilisateurs/:id/
 utilisateursRouter.patch("/:id", (req, res) => useRest(patchUtilisateur, req, res));
 // DELETE /utilisateurs/:id/
-utilisateursRouter.get("/", (req, res) => useRest(deleteUtilisateur, req, res));
+utilisateursRouter.delete("/", (req, res) => useRest(deleteUtilisateur, req, res));
 
 export { utilisateursRouter as utilisateursRouter };

b7aaea786b5db4add7e9f32e8019e5ebdc492f32
antoinedmns <antoine.damiens1@orange.fr>
2025-12-01 19:35:18 +0100

    création de la route roles

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { sessionsRouter } from "./sessions";
 import { utilisateursRouter } from "./utilisateurs";
+import { rolesRouteur } from "./roles";
 
 const router = Router();
 
 router.use("/sessions", sessionsRouter);
 router.use("/utilisateurs", utilisateursRouter);
+router.use("/roles", rolesRouteur)
 
 export { router as indexRouter };

2bd133950bd94e2dcac6bfe235d10277f9a90985
antoinedmns <antoine.damiens1@orange.fr>
2026-03-26 16:18:39 +0100

    fix contracts etudiants

--- src/contracts/etudiants.ts
+++ src/contracts/etudiants.ts
@@ -7,10 +7,11 @@ export const EtudiantSchema = z.object({
     prenom: z.string()
 });
 
-export const NewEtudiantSchema = EtudiantSchema.omit({ numeroEtudiant: true });
+export const NewEtudiantSchema = EtudiantSchema;
 
 export const UpdateEtudiantSchema = EtudiantSchema.pick({ nom: true, prenom: true }).partial();
 
 // --- Types ---
 export type APIEtudiant = z.infer<typeof EtudiantSchema>;
+export type APINewEtudiant = z.infer<typeof NewEtudiantSchema>;
 export type APIUpdateEtudiant = z.infer<typeof UpdateEtudiantSchema>;

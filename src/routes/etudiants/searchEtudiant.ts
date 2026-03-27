f1559d6c9dd089a2d64bc309fa6bb6841af9f641
antoinedmns <antoine.damiens1@orange.fr>
2026-03-27 17:17:56 +0100

    fix build

--- src/routes/etudiants/searchEtudiant.ts
+++
@@ -1,5 +0,0 @@
-import { APIEtudiant } from "../../contracts/etudiants";
-import { Database } from "../../core/services/database/Database";
-import { ErreurRequeteInvalide } from "../erreursApi";
-
-export async function searchEtudiant(query: string)

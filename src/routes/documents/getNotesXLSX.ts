7906bfb325d615bdcf716ca407489b39b413fb6b
antoinedmns <antoine.damiens1@orange.fr>
2026-04-07 16:18:42 +0200

    feat(API): refractor export notes

--- src/routes/documents/getNotesXLSX.ts
+++ src/routes/documents/getNotesXLSX.ts
@@ -2,6 +2,7 @@ import { sessionCache } from "../../cache/sessions/SessionCache"
 import XLSX from "@e965/xlsx";
 import { ErreurRequeteInvalide } from "../erreursApi"
 import { logInfo } from "../../utils/logger";
+import { Response } from 'express';
 
 export interface formatExport {
     code_anonymat: string,
@@ -9,23 +10,16 @@ export interface formatExport {
     note: number
 }
 
-export async function getNotesXLSX(sessionId: string, codeEpreuve: string): Promise<Buffer> {
+export async function getNotesXLSX(sessionId: string, codeEpreuve: string, res: Response): Promise<void> {
 
     const idSession = parseInt(sessionId ?? '');
-
-    if (isNaN(idSession)) {
-        throw new ErreurRequeteInvalide("L'id de la session est invalide.");
-    }
+    if (isNaN(idSession)) throw new ErreurRequeteInvalide("L'id de la session est invalide.");
 
     const session = await sessionCache.getOrFetch(idSession);
-    if (!session) {
-        throw new ErreurRequeteInvalide("Session introuvable.");
-    } 
+    if (!session) throw new ErreurRequeteInvalide("Session introuvable.");
 
     const epreuve = await session.epreuves.getOrFetch(codeEpreuve);
-    if (!epreuve) {
-        throw new ErreurRequeteInvalide("Épreuve introuvable.");
-    } 
+    if (!epreuve) throw new ErreurRequeteInvalide("Épreuve introuvable.");
 
     const convocations = await epreuve.convocations.getAll();
 
@@ -33,30 +27,56 @@ export async function getNotesXLSX(sessionId: string, codeEpreuve: string): Prom
 
     for (const convocation of convocations) {
 
-        const codeAnonymatExport = convocation.codeAnonymat.toString();
-        const numeroEtudiantExport = convocation.numeroEtudiant?.toString() ?? "null";
         const noteExport = convocation.noteQuart;
+        const codeAnonymatExport = convocation.codeAnonymat;
+        const numeroEtudiantExport = convocation.numeroEtudiant?.toString() ?? "";
 
-        if(noteExport != null) {
+        if (noteExport != null) {
             tableauExport.push({
-            code_anonymat: codeAnonymatExport,
-            numero_etudiant: numeroEtudiantExport,
-            note: noteExport / 4
-        });
+                code_anonymat: codeAnonymatExport,
+                numero_etudiant: numeroEtudiantExport,
+                note: noteExport / 4
+            });
         }
     }
 
-    logInfo("serveur", `Génération de ${tableauExport.length} lignes pour export-notes-${codeEpreuve}.xlsx`);
+    const convocationsSupplementaires = epreuve.convocations.convocationsSupplementaires.values();
+
+    for (const convocationSupplementaire of convocationsSupplementaires) {
+
+        const noteExport = convocationSupplementaire.noteQuart;
+        const codeAnonymatExport = convocationSupplementaire.codeAnonymat;
+        const numeroEtudiantExport = convocationSupplementaire.numeroEtudiant?.toString() ?? "";
+
+        if (codeAnonymatExport.startsWith("Z")) {
+            if (noteExport != null) {
+                tableauExport.push({
+                    code_anonymat: codeAnonymatExport,
+                    numero_etudiant: numeroEtudiantExport,
+                    note: noteExport / 4
+                });
+            }
+        }
+
+    }
 
     if (tableauExport.length === 0) {
         throw new ErreurRequeteInvalide("Aucune note n'est disponible pour cette épreuve.");
     }
 
+    logInfo("serveur", `Génération de ${tableauExport.length} lignes pour export-notes-${codeEpreuve}.xlsx`);
+
     const feuille = XLSX.utils.json_to_sheet(tableauExport);
     const classeur = XLSX.utils.book_new();
+
     XLSX.utils.book_append_sheet(classeur, feuille, "Notes");
 
     const buffer = XLSX.write(classeur, { type: 'buffer', bookType: 'xlsx' });
 
-    return buffer as Buffer;
+    res.setHeader(
+        'Content-Disposition',
+        `attachment; filename="export-notes-${codeEpreuve}.xlsx"`
+    );
+
+    res.send(buffer);
 }

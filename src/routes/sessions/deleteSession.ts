1791ad2551269c9b6dfbbbe263e25244fe16407f
antoinedmns <antoine.damiens1@orange.fr>
2026-04-11 16:43:31 +0200

    fix(API): route deleteSession

--- src/routes/sessions/deleteSession.ts
+++ src/routes/sessions/deleteSession.ts
@@ -1,11 +1,18 @@
 import { sessionCache } from "../../cache/sessions/SessionCache";
+import { APIBoolResponse } from "../../contracts/common";
+import { ErreurRequeteInvalide } from "../erreursApi";
 
 /**
  * Supprime une session.
  * @route `DELETE /sessions/:id/`
  */
-export async function deleteSession(sessionId: string): Promise<{ success: boolean }> {
+export async function deleteSession(sessionId: string): Promise<APIBoolResponse> {
     const idSession = parseInt(sessionId ?? '');
+
+    if(isNaN(idSession) || sessionId === undefined) {
+        throw new ErreurRequeteInvalide("L'id de la session est invalide.");
+    }
+    
     const suppressionSession = await sessionCache.delete(idSession);
 
     return {
